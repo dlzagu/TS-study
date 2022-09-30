cd1c7ce90716f63b0b693c8d9e14f4e38cd4ee04
dlzagu <eodnsdlekd@naver.com>
2022-09-30 21:31:44 +0900

    react에 typescript적용해보기

---
+++ shopping-app/src/components/Greeter.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface GreeterProps {
+  person: string;
+}
+
+function Greeter({ person }: GreeterProps) {
+  return (
+    <div>
+      <h1>Hello, {person}</h1>;
+    </div>
+  );
+}
+
+export default Greeter;
