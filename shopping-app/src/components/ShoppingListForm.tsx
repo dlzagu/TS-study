2741d00828487fc324bc30ea6c77174cdaf3e93c
dlzagu <eodnsdlekd@naver.com>
2022-10-03 23:54:50 +0900

    Typescript의 useRef

--- shopping-app/src/components/ShoppingListForm.tsx
+++ shopping-app/src/components/ShoppingListForm.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-
-function ShoppingListForm(): JSX.Element {
+import React, { useRef } from "react";
+interface ShoppingListFormProps {
+  onAddItem: (item: string) => void;
+}
+function ShoppingListForm({ onAddItem }: ShoppingListFormProps): JSX.Element {
+  const inputRef = useRef<HTMLInputElement>(null);
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    console.log("SUBMITTED!!");
+    const newProduct = inputRef.current!.value;
+    onAddItem(newProduct);
+    inputRef.current!.value = "";
   }
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Product Name" />
+      <input type="text" placeholder="Product Name" ref={inputRef} />
       <button type="submit">Add item</button>
     </form>
   );
